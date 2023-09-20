d383bc85c5fab1b6244ed1a0328c63cf0dacb255
DavidCM <dcm.robin@gmail.com>
2023-09-20 16:43:59 +0100

    init commit

---
+++ Assets/Scripts/PlanePilot.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlanePilot : MonoBehaviour
+{
+    //public bool usingTerrain = true;
+    public float speed = 10.0f;
+    public bool crashed;
+    public bool stalled;
+
+    private void FixedUpdate() {
+        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+        float bias = 0.96f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
+
+        if (!crashed)
+        {
+            if (!stalled)
+            {
+                transform.position += transform.forward * Time.deltaTime * speed;
+            }
+            speed -= transform.forward.y * Time.deltaTime * 50.0f;
+            transform.Rotate(Input.GetAxis("Vertical") * 2, 0.0f, -Input.GetAxis("Horizontal") * 2);
+        }
+
+        if (speed <= 0.0f)
+        {
+            speed = 0.0f;
+            stalled = true;
+            //speed = 35.0f;//not realistic... the engine should stall
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed/2, ForceMode.Impulse);
+        }
+        else
+        {
+            stalled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<Terrain>() != null || other.gameObject.CompareTag("Ground"))
+        {
+            if (!crashed)
+            {
+                crashed = true;
+                speed = 0;
+                GetComponent<Rigidbody>().isKinematic = false;
+                GetComponent<Rigidbody>().AddForce(transform.forward * speed/2, ForceMode.Impulse);
+            }
+        }
+    }
+}
