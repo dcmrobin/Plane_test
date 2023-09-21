362ca500594eca89cbccf85b05387dbb16c6cd40
DavidCM <dcm.robin@gmail.com>
2023-09-21 21:45:11 +0100

    experimenting with Unity Netcode (multiplayer)

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerControl: NetworkBehaviour
+{
+    [SerializeField]
+    private float speed = 0.5f;
+
+    void Update()
+    {
+        if (!IsOwner) return;
+
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xInput, 0, yInput).normalized;
+        transform.Translate(speed * Time.deltaTime * moveDirection);
+    }
+}
