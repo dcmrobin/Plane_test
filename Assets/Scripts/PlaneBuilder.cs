df295f2c4ff94de23cf97df84b6ebc678445980d
DavidCM <dcm.robin@gmail.com>
2023-09-27 12:33:59 +0100

    bug fixed... but some thing is wrong

--- Assets/Scripts/PlaneBuilder.cs
+++ Assets/Scripts/PlaneBuilder.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class PlaneBuilder : MonoBehaviour
+public class PlaneBuilder : NetworkBehaviour
 {
     [SerializeField] private GameObject wingsPrefab;
     [SerializeField] private GameObject tailfinLowPrefab;
     [SerializeField] private GameObject tailfinHighPrefab;
-    private void Start() {
+    private void Awake() {
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
     }
 
@@ -18,30 +18,22 @@ public class PlaneBuilder : MonoBehaviour
         {
             GameObject parentObject = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.gameObject;
             PlanePilot playerScript = parentObject.GetComponent<PlanePilot>();
-    
+
             GameObject spawnedwings = Instantiate(wingsPrefab, Vector3.zero, Quaternion.identity);
             spawnedwings.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
             spawnedwings.transform.SetParent(parentObject.transform);
-            spawnedwings.transform.position = playerScript.wings.transform.position;
-            spawnedwings.transform.rotation = playerScript.wings.transform.rotation;
-            Destroy(playerScript.wings);
-            playerScript.wings = spawnedwings;
+            //playerScript.curwings = spawnedwings;
 
             GameObject spawnedtailfinLow = Instantiate(tailfinLowPrefab, Vector3.zero, Quaternion.identity);
             spawnedtailfinLow.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
             spawnedtailfinLow.transform.SetParent(parentObject.transform);
-            spawnedtailfinLow.transform.position = playerScript.tailfinLow.transform.position;
-            spawnedtailfinLow.transform.rotation = playerScript.tailfinLow.transform.rotation;
-            Destroy(playerScript.tailfinLow);
-            playerScript.tailfinLow = spawnedtailfinLow;
+            //playerScript.curtailLow = spawnedtailfinLow;
 
             GameObject spawnedtailfinHigh = Instantiate(tailfinHighPrefab, Vector3.zero, Quaternion.identity);
             spawnedtailfinHigh.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
             spawnedtailfinHigh.transform.SetParent(parentObject.transform);
-            spawnedtailfinHigh.transform.position = playerScript.tailfinHigh.transform.position;
-            spawnedtailfinHigh.transform.rotation = playerScript.tailfinHigh.transform.rotation;
-            Destroy(playerScript.tailfinHigh);
-            playerScript.tailfinHigh = spawnedtailfinHigh;
+            //playerScript.curtailHigh = spawnedtailfinHigh;
+            playerScript.AssignClientRpc();
         }
     }
 }
--- Assets/Scripts/PlanePilot.cs
+++ Assets/Scripts/PlanePilot.cs
@@ -5,11 +5,13 @@ public class PlanePilot : NetworkBehaviour
 {
     //public bool usingTerrain = true;
     public float speed = 10.0f;
+    [Header("Gun params")]
     public int gunDamage = 2;
+    public LayerMask targetMask;
+    [Header("Plane status")]
     public bool crashed;
     public bool stalled;
     public bool shotDown;
-    public LayerMask targetMask;
     RaycastHit hit;
     [Header("Bits of the plane")]
     public GameObject body;
@@ -17,42 +19,42 @@ public class PlanePilot : NetworkBehaviour
     public GameObject tailfinLow;
     public GameObject tailfinHigh;
     private bool slowingDown;
-    /*[Header("Prefabs")]
-    [SerializeField] private GameObject wingsPrefab;
-    [SerializeField] private GameObject tailfinLowPrefab;
-    [SerializeField] private GameObject tailfinHighPrefab;
+    public GameObject curwings;
+    public GameObject curtailLow;
+    public GameObject curtailHigh;
 
-    private void Start() {
-        SpawnWingsServerRpc();
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    public void SpawnWingsServerRpc()
-    {
-        GameObject spawnedwings = Instantiate(wingsPrefab, Vector3.zero, Quaternion.identity);
-        spawnedwings.GetComponent<NetworkObject>().Spawn();
-        spawnedwings.transform.SetParent(transform);
-        spawnedwings.transform.position = wings.transform.position;
-        spawnedwings.transform.rotation = wings.transform.rotation;
-        Destroy(wings);
-        wings = spawnedwings;
-
-        GameObject spawnedtailfinLow = Instantiate(tailfinLowPrefab, Vector3.zero, Quaternion.identity);
-        spawnedtailfinLow.GetComponent<NetworkObject>().Spawn();
-        spawnedtailfinLow.transform.SetParent(transform);
-        spawnedtailfinLow.transform.position = tailfinLow.transform.position;
-        spawnedtailfinLow.transform.rotation = tailfinLow.transform.rotation;
-        Destroy(tailfinLow);
-        tailfinLow = spawnedtailfinLow;
-
-        GameObject spawnedtailfinHigh = Instantiate(tailfinHighPrefab, Vector3.zero, Quaternion.identity);
-        spawnedtailfinHigh.GetComponent<NetworkObject>().Spawn();
-        spawnedtailfinHigh.transform.SetParent(transform);
-        spawnedtailfinHigh.transform.position = tailfinHigh.transform.position;
-        spawnedtailfinHigh.transform.rotation = tailfinHigh.transform.rotation;
-        Destroy(tailfinHigh);
-        tailfinHigh = spawnedtailfinHigh;
+    /*private void Start() {
+        if (IsOwner)
+        {
+            
+    
+            
+        }
     }*/
+    [ClientRpc]
+    public void AssignClientRpc()
+    {
+        crashed = false;
+        stalled = false;
+        shotDown = false;
+
+        curwings = transform.Find(wings.name + "(Clone)").gameObject;
+        curtailLow = transform.Find(tailfinLow.name + "(Clone)").gameObject;
+        curtailHigh = transform.Find(tailfinHigh.name + "(Clone)").gameObject;
+
+        curwings.transform.position = wings.transform.position;
+        //Destroy(wings);
+        wings = curwings;
+    
+        curtailLow.transform.position = tailfinLow.transform.position;
+        //Destroy(tailfinLow);
+        tailfinLow = curtailLow;
+    
+        curtailHigh.transform.position = tailfinHigh.transform.position;
+        curtailHigh.transform.rotation = tailfinHigh.transform.rotation;
+        //Destroy(tailfinHigh);
+        tailfinHigh = curtailHigh;
+    }
     private void FixedUpdate() {
         if (!IsOwner) return;
 
